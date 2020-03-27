990d06d9dcd848084ccc2cd24ee339f5a6a018ac
Emily <emily@laptop-4qsrktd7.home>
2020-03-27 13:17:08 +0000

    Adds actions and routes for most regions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,4 +5,22 @@ class PagesController < ApplicationController
     def about
     end
 
+    def eastasia
+    end 
+
+    def europe
+    end 
+
+    def westasia
+    end 
+
+    def southamerica
+    end 
+
+    def southandsoutheastasia
+    end 
+
+    def americanamerican
+    end 
+
 end
