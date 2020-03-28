77506214396b3b1bdc29dc745527ea400309f0e3
Emily <emilyspencer89@hotmail.co.uk>
2020-03-28 20:56:51 +0000

    Adds timestamps to reviews

---
+++ db/migrate/20200328203943_remove_time_stamp_from_reviews.rb
@@ -0,0 +1,11 @@
+class RemoveTimeStampFromReviews < ActiveRecord::Migration[6.0]
+  def change
+    remove_timestamps :reviews, null: true
+  
+    #long_ago = DateTime.new(2020, 3, 28)
+    #Review.update_all(created_at: long_ago, updated_at: long_ago)
+  
+    #change_column_null :reviews, :created_at, false
+    #change_column_null :reviews, :updated_at, false
+  end
+end
