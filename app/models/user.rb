b7908dab64f111394e8dc56b65bbe6fc3ccc08bf
Emily <emilyspencer89@hotmail.co.uk>
2020-03-28 15:10:07 +0000

    Adds user credentials and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
+    validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 4, maximum: 25 }
+    VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false }, 
+    format: { with: VALID_EMAIL }
+    before_save { self.email = email.downcase }
 
 end 
