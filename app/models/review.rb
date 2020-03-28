09db35b28dbfb11a2117339c4a26f701aaaf602f
Emily <emilyspencer89@hotmail.co.uk>
2020-03-28 18:15:21 +0000

    Makes a one-to-many association between users and reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,5 @@
 class Review < ActiveRecord::Base
+    belongs_to :user
     validates :title, presence: true, length: { minimum: 3, maximum: 50 }
     validates :review, presence: true, length: { minimum: 20, maximum: 1000 }
 end
