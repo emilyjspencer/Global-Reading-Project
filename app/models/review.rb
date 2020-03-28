011546cd1c38d3023f7672cea75d128e28576532
Emily <emilyspencer89@hotmail.co.uk>
2020-03-28 18:21:25 +0000

    Each review is connected to a particular user

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,5 @@ class Review < ActiveRecord::Base
     belongs_to :user
     validates :title, presence: true, length: { minimum: 3, maximum: 50 }
     validates :review, presence: true, length: { minimum: 20, maximum: 1000 }
+    validates :user_id, presence: true
 end
